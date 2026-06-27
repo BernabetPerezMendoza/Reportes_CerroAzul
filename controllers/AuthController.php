0d41d6268f2776ac05bc76bcb36495f82970a4bb
Carlos Sanchez <CarlosSanchezAlcala170103@outlook.com>
2026-06-27 14:38:41 -0500

    feat: BPM-01 solved errors

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use User;
+
 
 require_once __DIR__ . '/../models/User.php';
 
--- models/Report.php
+++ models/Report.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Database;
 
 require_once __DIR__ . '/../config/Database.php';
 
